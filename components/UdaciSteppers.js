ec8128c847593b6a416d05f6c0d526c6a4855871
gitzart <windowxpxp@gmail.com>
2017-09-24 18:32:52 +0630

    Add steppers

--- components/UdaciSteppers.js
+++ components/UdaciSteppers.js
@@ -1,10 +1,24 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { FontAwesome, Entypo } from '@expo/vector-icons'
 
-export default function UdaciSteppers () {
+export default function UdaciSteppers ({ onIncrement, onDecrement, value, unit }) {
   return (
     <View>
-      <Text>UdaciSteppers</Text>
+      <View>
+        <TouchableOpacity onPress={onIncrement}>
+          <FontAwesome name='plus' size={30} color={'black'} />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={onDecrement}>
+          <FontAwesome name='minus' size={30} color={'black'} />
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
   )
 }
