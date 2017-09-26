f6a878948fd0f3e4f33b0afc4784dc9691090f28
gitzart <windowxpxp@gmail.com>
2017-09-26 20:14:11 +0630

    Style components

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
-import { View, TouchableOpacity, Text } from 'react-native'
+import { View, TouchableOpacity, Text, StyleSheet, Platform } from 'react-native'
 import { connect } from 'react-redux'
 import {
   getMetricMetaInfo, timeToString, getDailyReminderValue
 } from '../utils/helpers'
 import { addEntry } from '../actions'
+import { purple, white } from '../utils/colors'
 import { submitEntry, removeEntry } from '../utils/api'
 import { Ionicons } from '@expo/vector-icons'
 import UdaciSteppers from './UdaciSteppers'
 import UdaciSlider from './UdaciSlider'
 import DateHeader from './DateHeader'
 import TextButton from './TextButton'
+
+function SubmitBtn ({ onPress }) {
+  return (
+    <TouchableOpacity
+      style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
+      onPress={onPress}>
+        <Text style={styles.submitBtnText}>SUBMIT</Text>
+    </TouchableOpacity>
+  )
+}
 
 class AddEntry extends Component {
   state = {
@@ -76,10 +87,13 @@ class AddEntry extends Component {
 
     if (this.props.alreadyLogged) {
       return (
-        <View>
-          <Ionicons name='ios-happy-outline' size={100} />
+        <View style={styles.center}>
+          <Ionicons
+            name={Platform.OS === 'ios' ? 'ios-happy-outline' : 'md-happy'}
+            size={100}
+          />
           <Text>You already logged your information for today.</Text>
-          <TextButton onPress={this.reset}>
+          <TextButton style={{ padding: 10 }} onPress={this.reset}>
             Reset
           </TextButton>
         </View>
@@ -87,14 +101,14 @@ class AddEntry extends Component {
     }
 
     return (
-      <View>
+      <View style={styles.container}>
         <DateHeader date={new Date().toLocaleDateString()} />
         {Object.keys(metaInfo).map(key => {
           const { getIcon, type, ...rest } = metaInfo[key]
           const value = this.state[key]
 
           return (
-            <View key={key}>
+            <View key={key} style={styles.row}>
               {getIcon()}
               {type === 'slider'
                 ? <UdaciSlider
@@ -112,13 +126,55 @@ class AddEntry extends Component {
             </View>
           )
         })}
-        <TextButton onPress={this.submit}>
-          Submit
-        </TextButton>
+        <SubmitBtn onPress={this.submit} />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  iosSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40
+  },
+  AndroidSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    borderRadius: 2,
+    height: 45,
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center',
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30
+  }
+})
 
 function mapStateToProps (state) {
   const key = timeToString()
--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,18 +1,35 @@
 import React from 'react'
-import { View, Text, Slider } from 'react-native'
+import { View, Text, Slider, StyleSheet } from 'react-native'
+import { gray } from '../utils/colors'
 
 export default function UdaciSlider ({ step, value, onChange, max, unit }) {
   return (
-    <View>
+    <View style={styles.row}>
       <Slider
+        style={{ flex: 1 }}
         value={value}
         onValueChange={onChange}
         maximumValue={max}
         miniumValue={0}
         step={step}
       />
-      <Text>{value}</Text>
-      <Text>{unit}</Text>
+      <View style={styles.metricCounter}>
+        <Text style={{ fontSize: 24, textAlign: 'center' }}>{value}</Text>
+        <Text style={{ fontSize: 18, color: gray }}>{unit}</Text>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  metricCounter: {
+    width: 85,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
